31e234089960df48857a32e8ee18f8c02ed9f610
William Trevisan <william.trevisan@multi24h.com.br>
2025-03-23 21:11:17 -0300

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { theme } from './src/theme/theme';
+import { HomeScreen } from './src/screens/HomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <SafeAreaProvider>
+        <StatusBar style="light" backgroundColor="transparent" translucent />
+        <HomeScreen />
+      </SafeAreaProvider>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
